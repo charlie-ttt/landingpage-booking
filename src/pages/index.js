fd571c62aa178e0ad9fd3b368e8ffaf43a7a7b00
Charlie <achanont@gmail.com>
2021-08-22 19:38:15 +0700

    add seo title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import ProductSmokingHero from "../modules/views/ProductSmokingHero"
 import ProductValues from "../modules/views/ProductValues"
 // --- Post bootstrap -----
 import React from "react"
+import Seo from "../components/seo"
 import withRoot from "../modules/withRoot"
 
 function Index() {
   return (
     <React.Fragment>
+      <Seo title="Main Page" />
       <AppAppBar />
       <ProductHero />
       <ProductValues />
