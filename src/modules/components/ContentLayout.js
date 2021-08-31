0a3119fc08834fb9a03d2977b3222dd0fa426bea
Charlie <achanont@gmail.com>
2021-08-31 22:09:24 +0700

    fix content writing css

--- src/modules/components/ContentLayout.js
+++ src/modules/components/ContentLayout.js
@@ -10,43 +10,20 @@ const styles = theme => ({
     backgroundColor: theme.palette.secondary.light,
   },
   content: {
-    margin: [[0, 148, 0, 148]],
-    // padding: 24,
+    margin: "auto",
+    maxWidth: "42rem",
   },
 })
 
 const Layout = ({ location, title, children, classes }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
-
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      {/* <header className="global-header">{header}</header> */}
+    <div className={classes.root}>
       <AppAppBar />
-      <main className={classes.root}>
-        {/* <div className={classes.content}> */}
-        {children}
-        {/* </div> */}
+      <main>
+        <div className={classes.content}>{children}</div>
       </main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      <footer style={{ textAlign: "center" }}>
+        © Calendar Booking {new Date().getFullYear()}
       </footer>
     </div>
   )
