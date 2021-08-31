35744a3cf67913717280fbb2911ddc04126a1771
Charlie <achanont@gmail.com>
2021-08-31 22:43:39 +0700

    improve blog styling

--- src/modules/components/ContentLayout.js
+++ src/modules/components/ContentLayout.js
@@ -1,18 +1,25 @@
 import * as React from "react"
 
 import AppAppBar from "../views/AppAppBar"
+import Paper from "@material-ui/core/Paper"
+import productCurvyLines from "../../images/productCurvyLines.png"
 import withRoot from "../withRoot"
 import { withStyles } from "@material-ui/core/styles"
 
 const styles = theme => ({
   root: {
-    backgroundColor: theme.palette.secondary.light,
+    backgroundImage: `url(${productCurvyLines})`,
+    backgroundColor: "#fff5f8",
     paddingBottom: 20,
   },
   content: {
     margin: "auto",
     maxWidth: "42rem",
     minHeight: "95vh",
+  },
+  paper: {
+    padding: "5px 2rem",
+    margin: "2rem 0",
   },
   footer: {
     textAlign: "center",
@@ -24,7 +31,9 @@ const Layout = ({ location, title, children, classes }) => {
     <div className={classes.root}>
       <AppAppBar />
       <main>
-        <div className={classes.content}>{children}</div>
+        <div className={classes.content}>
+          <Paper className={classes.paper}>{children}</Paper>
+        </div>
       </main>
       <footer className={classes.footer}>
         © Calendar Booking {new Date().getFullYear()}
